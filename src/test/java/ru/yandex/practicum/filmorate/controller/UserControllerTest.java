c0683b67c5c32b220ff334e9a52bfe966db6fc69
mrmaximum80 <mrmaximum@mail.ru>
2023-04-29 16:20:08 +0300

    Вторая итерация на проверку 1. Исправлен pom.xml 2. Инкремент id объединен в одну строку 3. Добавлен тест для пустого имени пользователя.

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
