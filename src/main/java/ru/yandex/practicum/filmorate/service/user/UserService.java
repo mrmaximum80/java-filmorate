43ce0302890512ec26d05e8a7f32a700ca250806
mrmaximum80 <mrmaximum@mail.ru>
2023-06-03 00:46:42 +0300

    Вторая итерация на проверку Добавлены пробелы между скобками в ExceptionsHandler

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import javax.validation.Valid;
 import java.util.*;
 
 @Service
